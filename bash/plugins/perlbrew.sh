d1aef23b87810ad0bb6fd810abb55be79cec8b9b
Traian Nedelea <tron1point0@gmail.com>
2012-04-30 12:08:22 -0500

    bash completion for cpanm

--- bash/plugins/perlbrew.sh
+++ bash/plugins/perlbrew.sh
@@ -1,2 +1,6 @@
-export PERLBREW_ROOT=$HOME/Projects/perlbrew
-source $PERLBREW_ROOT/etc/bashrc
+if [ -d $HOME/Projects/perlbrew ]; then
+    export PERLBREW_ROOT=$HOME/Projects/perlbrew
+    source $PERLBREW_ROOT/etc/bashrc
+fi
+
+can setup-bash-complete && source setup-bash-complete
