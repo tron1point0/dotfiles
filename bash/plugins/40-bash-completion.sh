827f840e8aa88265df28e16e6571a9ce856cd328
Traian Nedelea <tron1point0@gmail.com>
2012-06-10 13:41:47 -0500

    one more place where bash_completion likes to be

--- bash/plugins/40-bash-completion.sh
+++ bash/plugins/40-bash-completion.sh
@@ -3,5 +3,8 @@
 [ -r /etc/bash_completion ] && \
 	source /etc/bash_completion
 
+[ -r /usr/local/etc/bash_completion ] && \
+	source /usr/local/etc/bash_completion
+
 [ -r /usr/share/bash-completion/bash_completion ] && \
 	source /usr/share/bash-completion/bash_completion
