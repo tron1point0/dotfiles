e9f4631b6d8beb2e5955a2f15ac43a4b07907b4e
Traian Nedelea <tron1point0@gmail.com>
2012-06-10 13:40:34 -0500

    perlbrew that can install itself

--- bash/plugins/perlbrew.sh
+++ bash/plugins/perlbrew.sh
@@ -1,6 +1,13 @@
 if [ -d $HOME/Projects/perlbrew ]; then
     export PERLBREW_ROOT=$HOME/Projects/perlbrew
     source $PERLBREW_ROOT/etc/bashrc
+else
+    function perlbrew {
+        unset -f perlbrew
+        export PERLBREW_ROOT=${1:-$HOME/Projects/perlbrew}
+        curl -kL http://install.perlbrew.pl | bash
+        source $PERLBREW_ROOT/etc/bashrc
+    }
 fi
 
-can setup-bash-complete && source setup-bash-complete
+can setup-bash-complete && source <(setup-bash-complete)
