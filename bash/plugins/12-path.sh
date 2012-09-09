8e15008d285b8a3597d9ef3a0b2379d7d6640ffb
Traian Nedelea <tron1point0@gmail.com>
2012-09-09 09:45:53 -0500

    List PATH

--- bash/plugins/12-path.sh
+++ bash/plugins/12-path.sh
@@ -13,13 +13,17 @@ function path {
             export PATH=$( ( IFS=: tokens $PATH ) | filter test $p != | ( FS=: untokens ))
         done
     ;;
+    -l)
+        tokens $(IFS=: ; echo $PATH)
+    ;;
     *)
         cat <<HELP
-USAGE: path -ar [paths...]
+USAGE: path -arl [paths...]
 OPTIONS:
     -a  Add to PATH
         If paths... already exist in PATH, they are brought to the front
     -r  Remove from PATH
+    -l  List paths in PATH, one per line
 HELP
     esac
 }
