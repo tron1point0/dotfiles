50c330c74ea9b852edea3996110bc08dbc612897
Tron <tron1point0@gmail.com>
2018-03-18 12:22:10 -0500

    Updated prompt for bash 4.4 features.

--- bash/plugins/90-prompt.sh
+++ bash/plugins/90-prompt.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 function __append_history {
-    declare -g +x H1="$(history 1 | cut -c 8-)"
     history -a
 }
 
@@ -14,16 +13,22 @@ function reset_term {
 
 function __update_prompt {
     local ret=$?
+
+    # Exit status
     local exitstatus=
-    local windowtitle="$(basename "$PWD")"
+    if [ $ret != 0 ] ; then
+        ret="$(printf '▲ %03d ▲' "$ret")"
+        local offset=$(( (COLUMNS - ${#ret} - 2) / 2 ))
+        exitstatus=$" \e[38;5;9m✘\e[${offset}C\e[4m$ret\e[24m\e[${offset}C✘\e[0m\n"
+    fi
 
     # Right status
-    local user_color='\e[38;5;11m'  # Yellow with no sudo
-    [ -n "$HAS_SUDO" ] && user_color='\e[38;5;7m'   # Grey with sudo
-    [ -w /etc/passwd ] && user_color='\e[38;5;9m'   # Red if rootish
-    local rightstatus="\
-\[${user_color}\]$USER\[\e[38;5;240m\]@\[\e[38;5;7m\]$HOSTNAME \
-\[\e[38;5;33m\]$(date +%R)"
+    local user_color=$'\e[38;5;11m'  # Yellow with no sudo
+    [ -n "$HAS_SUDO" ] && user_color=$'\e[38;5;7m'   # Grey with sudo
+    [ -w /etc/passwd ] && user_color=$'\e[38;5;9m'   # Red if rootish
+    local rightstatus='\
+\[${user_color}\]\u\[\e[38;5;240m\]@\[\e[38;5;7m\]\h \
+\[\e[38;5;33m\]\A'
 
     if command -v __git_ps1 >/dev/null ; then
         local GIT_PS1_SHOWDIRTYSTATE=1
@@ -36,37 +41,30 @@ function __update_prompt {
         rightstatus="$PS1"
     fi
 
-    # Strip non-printing escapes
-    [ -n "$1" ] && echo "$rightstatus"
-    local bare="$(echo "$rightstatus" | sed 's_\\\[__g;s_\\]__g;s_\\e\[\([0-9;]*\)m__g')"
-    eval 'bare="'$bare'"'   # Force variable expansion
+    rightstatus="${rightstatus@P}"
+    local bare="$(echo "$rightstatus" | sed $'s/\001[^\002]*\002//g')"
 
     local start=$(($COLUMNS - ${#bare} + 1))
     rightstatus="\e[${start}G$rightstatus"
 
-    PS1='\[\e7$(__prompt_exitstatus)\e8\]\
-\[\e]2;'${windowtitle}'\a\]\
+    PS1=${exitstatus}'\
+\[\e]2;'$(basename $PWD)'\a\]\
 \[\e7'${rightstatus}'\e8\]\
 \[\e[38;5;240m\]⎧\
 \[\e[1;38;5;33m\]\w\n\
 \[\e[0;38;5;240m\]⎩\[\e[0m\]\$ '
 }
 
-function __prompt_exitstatus {
-    local ret=$?
-
-    if [[ $ret != 0 ]] ; then
-        printf ' \e[38;5;9m︽ \e[4m%03d\e[24m\e[%dG︽\e[1S' $ret $((COLUMNS - 2))
-    fi
-}
-
-declare +x HAS_SUDO="$(expr "$(groups)" : '\b\(wheel\|admin\|staff\)\b')"
+declare +x HAS_SUDO="$(expr "$(groups)" : '\b\(wheel\|admin\|staff\|sudo\)\b')"
 
 PROMPT_COMMAND='__update_prompt ; __append_history ; reset_term'
 PROMPT_DIRTRIM=4
 
 PS2='\[\e7\e[1F\e[38;5;240m⎪\e8\]\
 \[\e[38;5;240m\]⎩\[\e[0m\]> '
+
+# Update the window title with the currently running command
+PS0='\[\e]2;$(history 1 | cut -c8-)\a\]'
 
 shopt -s checkwinsize   # Update LINES and COLUMNS after every command
 
