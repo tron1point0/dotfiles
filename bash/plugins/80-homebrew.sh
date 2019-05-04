1143a9ed6f09d8dea160caa5e2d0197f2b5c7a5f
Tron <tron1point0@gmail.com>
2019-05-04 11:27:04 -0500

    Made gnubin stuff generic.

--- bash/plugins/80-homebrew.sh
+++ bash/plugins/80-homebrew.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 if can brew ; then
-    COREUTILS='/usr/local/opt/coreutils'
-    path -a "$COREUTILS/libexec/gnubin"
-    modify-env -a add -v MANPATH -- "$COREUTILS/libexec/gnuman"
-    unset -v COREUTILS
+    BREWPREFIX='/usr/local/opt'
+    GNUBIN_PACKAGES=(
+        coreutils
+        inetutils
+    )
+
+    function add_gnubin_package {
+        local libexec_path="$1/libexec"
+        [[ -d "$libexec_path/gnubin" ]] && path -a "$libexec_path/gnubin"
+        [[ -d "$libexec_path/gnuman" ]] && modify-env -a add -v MANPATH "$libexec_path/gnuman"
+    }
+
+    for pkg in "${GNUBIN_PACKAGES[@]}" ; do
+        add_gnubin_package "$BREWPREFIX/$pkg"
+    done
+
     path -a '/usr/local/bin'
     path -a '/usr/local/sbin'
+
+    unset add_gnubin_package
+    unset -v GNUBIN_PACKAGES
+    unset -v BREWPREFIX
 fi
 
 true
