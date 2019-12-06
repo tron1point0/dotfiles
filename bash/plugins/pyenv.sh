8eaa6ee1eec208e4063d806c9f3034ca337f77fa
Tron <tron1point0@pm.me>
2019-12-06 10:33:58 -0600

    Use the same #! as all the others.

--- bash/plugins/pyenv.sh
+++ bash/plugins/pyenv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Much faster overrides for pyenv's default shell manipulations
 
@@ -10,6 +10,7 @@ if can pyenv ; then
         local version_file="$(parent-search .python-version)"
 
         if [[ -r "$version_file" ]] ; then
+            # TODO: Fix cding directly to directory with different python version file
             if [[ ! -v VIRTUAL_ENV ]] ; then
                 local python_version
                 read python_version < "$version_file"
