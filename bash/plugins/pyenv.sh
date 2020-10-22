9051ee8b9cfd8bfd1479150f309b34eaaf9bf529
Tron <tron1point0@pm.me>
2020-10-22 09:04:46 -0500

    Fixed pyenv cding between directories. Added tracing to bashrc.

--- bash/plugins/pyenv.sh
+++ bash/plugins/pyenv.sh
@@ -6,19 +6,30 @@ if can pyenv ; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PYENV_SHELL=bash
 
-    function __pyenv_prompt {
-        local version_file="$(parent-search .python-version)"
+    function __pyenv_deactivate {
+        command -v deactivate >/dev/null && deactivate
+        unset PYENV_VERSION_FILE
+    }
 
-        if [[ -r "$version_file" ]] ; then
-            # TODO: Fix cding directly to directory with different python version file
-            if [[ ! -v VIRTUAL_ENV ]] ; then
-                local python_version
-                read python_version < "$version_file"
-                local activate="$PYENV_ROOT/versions/$python_version/bin/activate"
-                [[ -r "$activate" ]] && source "$activate"
-            fi
-        else
-            [[ -v VIRTUAL_ENV ]] && command -v deactivate >/dev/null && deactivate
+    function __pyenv_activate {
+        local version file="$1"
+        read version < "$file"
+        local activate="$PYENV_ROOT/versions/$version/bin/activate"
+        if [[ -r "$activate" ]] ; then
+            source "$activate"
+            PYENV_VERSION_FILE="$file"
         fi
+    }
+
+    function __pyenv_prompt {
+        # We're in a venv and still in a descendant dir
+        [[ -v VIRTUAL_ENV && "$PWD" = "${PYENV_VERSION_FILE%/\.python-version}"* ]] && return 0
+
+        # We've in a venv, but outside the one containing our `.python-version`
+        [[ -v VIRTUAL_ENV ]] && __pyenv_deactivate
+
+        # We're not in a venv at all
+        local file="$(parent-search .python-version)"
+        [[ -r "$file" ]] && __pyenv_activate "$file"
     }
 fi
