2c563ada07bdd22d771c355d12cf7bd3213c035f
Tron <tron1point0@pm.me>
2022-01-07 20:39:46 -0600

    Finally join the 90s with ls aliases

--- bash/plugins/99-alias.sh
+++ bash/plugins/99-alias.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 alias ls='ls -v --color'
+alias ll='ls -l'
+alias la='ls -lA'
+alias l='ls'
 ls --help | grep [-][-]group-directories-first \
     | column -t | cut -f1 1>/dev/null && \
     alias ls='ls -v --color --group-directories-first'
