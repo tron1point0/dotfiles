990191257038333668c4763a6408a9200820451a
Tron <tron1point0@pm.me>
2022-01-10 15:43:33 -0600

    Use exa if available

--- bash/plugins/99-alias.sh
+++ bash/plugins/99-alias.sh
@@ -2,14 +2,12 @@
 
 alias ls='ls -v --color'
 alias ll='ls -l'
-alias la='ls -lA'
+alias la='ls -la'
 alias l='ls'
-ls --help | grep [-][-]group-directories-first \
-    | column -t | cut -f1 1>/dev/null && \
-    alias ls='ls -v --color --group-directories-first'
 can axel && alias axel='axel -a'
 can tmux && alias screen='tmux attach'
 can xdg-open && alias open='xdg-open'
 can nvim && alias vim=nvim
+can exa && alias ls='exa -F --group-directories-first'
 
 true
