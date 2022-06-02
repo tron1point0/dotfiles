3c7f24804882014dab5280791012930958199873
Tron <tron1point0@pm.me>
2022-06-02 11:47:22 -0500

    Fix cd tab completion on fish

--- fish/conf.d/homebin.fish
+++ fish/conf.d/homebin.fish
@@ -2,5 +2,8 @@
 if status is-interactive
     [ -d "$HOME/.bin" ] && set -p PATH "$HOME/.bin"
     [ -d "$HOME/Projects" ] && set -p CDPATH "$HOME/Projects"
+
+    # Also prepend the current directory for tab completion
+    set -p CDPATH .
 end
 
