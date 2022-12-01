f808e059ed5dd17d680ce93fc9799719067e9c30
Tron <tron1point0@pm.me>
2022-12-01 09:19:23 -0600

    Per-host overrides for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,8 +5,8 @@ local config = {
   hide_tab_bar_if_only_one_tab = true,
   use_resize_increments = true,
   window_background_opacity = 0.9,
-  cursor_thickness = '4px',
-  underline_thickness = '3px',
+  cursor_thickness = '0.1cell',
+  underline_thickness = '0.1cell',
   window_padding = {
     top = 0,
     right = 0,
@@ -18,9 +18,10 @@ local config = {
   font = wezterm.font_with_fallback {
     'FiraCode Nerd Font',
     'Fira Code',
+    'Liberation Mono',
     'Menlo'
   },
-  font_size = 14,
+  font_size = 14.0,
 
   -- Colors
   -- color_scheme = 'OneDark (base16)',
@@ -57,5 +58,20 @@ local config = {
     -- }}}
   },
 }
+
+-- {{{ Per-host overrides
+local overrides = {
+  hadar = function()
+    config.font_size = 12.0
+  end,
+}
+
+local hostname = wezterm.hostname()
+for host, fn in pairs(overrides) do
+  if hostname == host then
+    fn()
+  end
+end
+-- }}}
 
 return config
