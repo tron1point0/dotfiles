b7a0afeafe22411cfe90719903ac0f3f4ba33fe8
Tron <tron@nedelea.me>
2022-12-04 18:43:10 -0600

    Added ~/.local/bin to the path if it exists

--- fish/conf.d/homebin.fish
+++ fish/conf.d/homebin.fish
@@ -1,6 +1,7 @@
 # Use ~/.bin if it exists
 if status is-interactive
     [ -d "$HOME/.bin" ] && set -p PATH "$HOME/.bin"
+    [ -d "$HOME/.local/bin" ] && set -p PATH "$HOME/.local/bin"
     [ -d "$HOME/Projects" ] && set -p CDPATH "$HOME/Projects"
 
     # Also prepend the current directory for tab completion
