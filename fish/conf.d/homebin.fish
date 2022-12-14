640c8b958a09bb5bb6cb698f5644a9716af096ae
Tron <tron1point0@pm.me>
2022-12-14 12:34:16 -0600

    Use fish_add_path instead of set -p PATH

--- fish/conf.d/homebin.fish
+++ fish/conf.d/homebin.fish
@@ -1,7 +1,7 @@
 # Use ~/.bin if it exists
 if status is-interactive
-    [ -d "$HOME/.bin" ] && set -p PATH "$HOME/.bin"
-    [ -d "$HOME/.local/bin" ] && set -p PATH "$HOME/.local/bin"
+    [ -d "$HOME/.bin" ] && fish_add_path -Pp "$HOME/.bin"
+    [ -d "$HOME/.local/bin" ] && fish_add_path -Pp "$HOME/.local/bin"
     [ -d "$HOME/Projects" ] && set -p CDPATH "$HOME/Projects"
 
     # Also prepend the current directory for tab completion
--- fish/conf.d/rust.fish
+++ fish/conf.d/rust.fish
@@ -1,6 +1,6 @@
 # Include any rust packaged binaries in PATH (if they exist)
 
 if status is-interactive && [ -d "$HOME/.cargo/bin" ]
-    set -p PATH "$HOME/.cargo/bin"
+    fish_add_path -Pp "$HOME/.cargo/bin"
 end
 
