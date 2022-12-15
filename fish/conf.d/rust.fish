c26c815eba1f685208031d0883a75961cf2cf140
Tron <tron@nedelea.me>
2022-12-14 18:23:30 -0600

    No need to modify PATH directly - just use fish_user_paths

--- fish/conf.d/rust.fish
+++ fish/conf.d/rust.fish
@@ -1,6 +1,6 @@
 # Include any rust packaged binaries in PATH (if they exist)
 
 if status is-interactive && [ -d "$HOME/.cargo/bin" ]
-    fish_add_path -Pm "$HOME/.cargo/bin"
+    fish_add_path -g "$HOME/.cargo/bin"
 end
 
