889e20d9920aec5cfc6018597b5a09208a7166f7
Tron <tron@nedelea.me>
2023-02-13 21:53:32 -0600

    Tmux-like tab bar for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,107 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
+wezterm.GLOBAL.username = (function()
+  local f = io.popen("whoami")
+  local value = f:read("*a")
+  f:close()
+  return value:gsub("%s", "")
+end)()
+
+wezterm.on('update-status', function(window, pane)
+  local username = wezterm.GLOBAL.username
+  local hostname = wezterm.hostname()
+  local time = wezterm.strftime '%H:%M'
+
+  local tab_index = 0
+  for _, t in pairs(window:mux_window():tabs_with_info()) do
+    if t.is_active then
+      tab_index = t.index + 1 -- TODO: Get the offset from tab_and_split_indices_are_zero_based
+      break
+    end
+  end
+
+  local pane_index = 0
+  for _, p in pairs(pane:tab():panes_with_info()) do
+    if p.is_active then
+      pane_index = p.index + 1 -- TODO: Get the offset from tab_and_split_indices_are_zero_based
+      break
+    end
+  end
+
+  window:set_left_status(wezterm.format {
+    { Foreground = { Color = '#000000' } },
+    { Background = { Color = '#5fd787' } },
+    { Text = ' ' .. window:window_id() .. ':' .. tab_index .. '.' .. pane_index .. ' ' },
+    'ResetAttributes',
+    { Foreground = { Color = '#5fd787' } },
+    { Text = wezterm.nerdfonts.ple_lower_left_triangle }
+  })
+
+  window:set_right_status(wezterm.format {
+    'ResetAttributes',
+    { Foreground = { Color = '#444444' } },
+    { Text = wezterm.nerdfonts.ple_lower_right_triangle },
+    { Foreground = { AnsiColor = 'Silver' } },
+    { Background = { Color = '#444444' } },
+    { Text = ' ' .. username },
+    { Foreground = { Color = '#6c6c6c' } },
+    { Text = '@' },
+    { Foreground = { AnsiColor = 'Silver' } },
+    { Text = hostname .. ' ' },
+    { Foreground = { Color = '#0087ff' } },
+    { Text = wezterm.nerdfonts.ple_lower_right_triangle },
+    { Foreground = { Color = 'white' } },
+    { Background = { Color = '#0087ff' } },
+    { Text = ' ' .. time .. ' ' },
+  })
+end)
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local title = tab.active_pane.title
+  if config.show_tab_index_in_tab_bar then
+    local offset = config.tab_and_split_indices_are_zero_based and 0 or 1
+    title = (tab.tab_index + offset) .. ':' .. title
+  end
+
+  -- TODO: Make the index separate and a darker color from the window title
+  -- TODO: Replace the : with an indicator of whether there is unseen output
+  -- (-,*,???)
+  -- It should look like this: `1-blah`, `1*blah`
+
+  -- One space on either side for the \/
+  -- One space on either side for padding
+  -- == 2 + 2 == 4
+  title = ' ' .. wezterm.truncate_right(title, max_width - 4) .. ' '
+
+  if tab.is_active then
+    return {
+      { Background = { Color = '#000000' } },
+      { Foreground = { Color = '#333333' } },
+      { Text = wezterm.nerdfonts.ple_lower_left_triangle },
+      'ResetAttributes',
+      { Foreground = { AnsiColor = 'Silver' } },
+      { Text = title },
+      'ResetAttributes',
+      { Background = { Color = '#000000' } },
+      { Foreground = { Color = '#333333' } },
+      { Text = wezterm.nerdfonts.ple_lower_right_triangle },
+    }
+  end
+
+  return {
+    { Text = ' ' .. title .. ' ' },
+  }
+end)
+
 local config = {
   -- Options
   hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
+  -- use_fancy_tab_bar = true,
+  tab_bar_at_bottom = true,
+  tab_max_width = 24,
+  status_update_interval = 200, -- TODO: Reset to default
   use_resize_increments = true,
   window_background_opacity = 0.9,
   cursor_thickness = '0.1cell',
