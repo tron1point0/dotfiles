22e647214d87bad54e9314527ed45208e2bc192a
Tron <tron@nedelea.me>
2025-05-21 16:49:30 -0500

    Add Lua community pack

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
 }
