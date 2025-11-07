d95dba5de70668b77174ae2beccdac44bcd68277
Tron <tron@nedelea.me>
2025-11-07 13:13:31 -0600

    Fix BROWSER for macos hosts

--- fish/conf.d/vars.fish
+++ fish/conf.d/vars.fish
@@ -3,6 +3,8 @@
 type -q elinks && set -gx BROWSER elinks
 type -q google-chrome && set -gx BROWSER google-chrome
 type -q firefox && set -gx BROWSER firefox
+type -q xdg-open && set -gx BROWSER xdg-open
+type -q open && set -gx BROWSER open
 
 type -q vim && set -gx EDITOR vim
 type -q nvim && set -gx EDITOR nvim
